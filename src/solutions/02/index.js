f81a196c3b1ed4923aa51f2f58c8843fb50456e0
Adrien Pyke <adrien.pyke@034gc.onmicrosoft.com>
2019-12-02 09:57:39 -0500

    refactor

--- src/solutions/02/index.js
+++ src/solutions/02/index.js
@@ -3,34 +3,28 @@ import dedent from 'dedent';
 
 const getInput = () => input.split(',').map(Number);
 
-const run = ints => {
-  let position = 0;
-  while (ints[position] !== 99) {
-    const op = ints[position];
-    const num1 = ints[ints[position + 1]];
-    const num2 = ints[ints[position + 2]];
-    const outputPos = ints[position + 3];
+const run = (noun, verb) => {
+  const ints = getInput();
+  ints[1] = noun;
+  ints[2] = verb;
+  let pos = 0;
+  while (ints[pos] !== 99) {
+    const [op, num1Pos, num2Pos, outputPos] = ints.slice(pos, pos + 4);
+    const num1 = ints[num1Pos];
+    const num2 = ints[num2Pos];
     ints[outputPos] = op === 1 ? num1 + num2 : num1 * num2;
-    position += 4;
+    pos += 4;
   }
   return ints[0];
 };
 
 export default {
-  part1() {
-    const ints = getInput();
-    ints[1] = 12;
-    ints[2] = 2;
-    return 'Result of program: ' + run(ints);
-  },
+  part1: () => 'Result of program: ' + run(12, 2),
   part2() {
     const expected = 19690720;
     for (let noun = 0; noun <= 99; noun++) {
       for (let verb = 0; verb <= 99; verb++) {
-        const ints = getInput();
-        ints[1] = noun;
-        ints[2] = verb;
-        if (run(ints) === expected) {
+        if (run(noun, verb) === expected) {
           return dedent`
             Result is ${expected} with noun ${noun} and verb ${verb}.
             100 * noun + verb: ${100 * noun + verb}.
