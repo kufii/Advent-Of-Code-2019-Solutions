68630c09d06ad5cba663001311b82427f92f3293
Adrien Pyke <adrien.pyke@034gc.onmicrosoft.com>
2019-12-04 10:12:22 -0500

    minor day 3 refactor

--- src/solutions/04/index.js
+++ src/solutions/04/index.js
@@ -1,7 +1,7 @@
 import input from './input';
 import { range } from '../../util';
 
-const parseInput = () => range(...input.split('-').map(Number));
+const parseInput = () => range(...input.split('-').map(Number)).map(String);
 
 const meets = pass => pass.match(/(\d)\1/u) && pass === [...pass].sort().join('');
 
@@ -13,14 +13,6 @@ const meets2 = pass =>
     .some(str => str.length === 2);
 
 export default {
-  part1: () =>
-    '# of passwords that meet criteria: ' +
-    parseInput()
-      .map(String)
-      .filter(meets).length,
-  part2: () =>
-    '# of passwords that meet criteria: ' +
-    parseInput()
-      .map(String)
-      .filter(meets2).length
+  part1: () => '# of passwords that meet criteria: ' + parseInput().filter(meets).length,
+  part2: () => '# of passwords that meet criteria: ' + parseInput().filter(meets2).length
 };
