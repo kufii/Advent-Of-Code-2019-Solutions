ba88b5ac7688b29c85f2bd2b7b7aa8ad1839da7e
Adrien Pyke <adrien.pyke@034gc.onmicrosoft.com>
2019-12-05 12:02:08 -0500

    day 5

---
+++ src/solutions/05/index.js
@@ -0,0 +1,76 @@
+import input from './input';
+
+const parseInput = () => input.split(',').map(Number);
+
+const getNum = (nums, mode, num) => (mode === 1 ? num : nums[num]);
+
+const OPS = {
+  1: {
+    fn: (nums, [mode1, mode2], num1, num2, address) =>
+      (nums[address] = getNum(nums, mode1, num1) + getNum(nums, mode2, num2)),
+    params: 3
+  },
+  2: {
+    fn: (nums, [mode1, mode2], num1, num2, address) =>
+      (nums[address] = getNum(nums, mode1, num1) * getNum(nums, mode2, num2)),
+    params: 3
+  },
+  3: {
+    fn: (nums, _, address, input) => (nums[address] = input),
+    params: 1
+  },
+  4: {
+    fn: (nums, [mode], num) => getNum(nums, mode, num),
+    params: 1,
+    output: true
+  },
+  5: {
+    fn: (nums, [mode1, mode2], num1, num2) =>
+      getNum(nums, mode1, num1) ? getNum(nums, mode2, num2) : null,
+    params: 2,
+    flow: 1
+  },
+  6: {
+    fn: (nums, [mode1, mode2], num1, num2) =>
+      !getNum(nums, mode1, num1) ? getNum(nums, mode2, num2) : null,
+    params: 2,
+    flow: 1
+  },
+  7: {
+    fn: (nums, [mode1, mode2], num1, num2, address) =>
+      (nums[address] = getNum(nums, mode1, num1) < getNum(nums, mode2, num2) ? 1 : 0),
+    params: 3
+  },
+  8: {
+    fn: (nums, [mode1, mode2], num1, num2, address) =>
+      (nums[address] = getNum(nums, mode1, num1) === getNum(nums, mode2, num2) ? 1 : 0),
+    params: 3
+  }
+};
+
+const readOp = code =>
+  code
+    .toString()
+    .padStart(5, '0')
+    .match(/(\d)(\d)(\d)(\d\d)/u)
+    .slice(1)
+    .map(Number)
+    .reverse();
+
+const run = (program, input) => {
+  const output = [];
+  let pos = 0;
+  while (pos < program.length && program[pos] !== 99) {
+    const [op, ...modes] = readOp(program[pos]);
+    const params = program.slice(pos + 1, pos + 1 + OPS[op].params);
+    const result = OPS[op].fn(program, modes, ...params, input);
+    if (OPS[op].output) output.push(result);
+    pos = OPS[op].flow && result != null ? result : pos + 1 + OPS[op].params;
+  }
+  return output;
+};
+
+export default {
+  part1: () => 'Diagnostic code with input 1: ' + run(parseInput(), 1).pop(),
+  part2: () => 'Diagnostic code with input 5: ' + run(parseInput(), 5).pop()
+};
--- src/solutions/index.js
+++ src/solutions/index.js
@@ -2,5 +2,6 @@ import day1 from './01';
 import day2 from './02';
 import day3 from './03';
 import day4 from './04';
+import day5 from './05';
 
-export default [day1, day2, day3, day4];
+export default [day1, day2, day3, day4, day5];
