ad26520a9b652d504c068eaaf67ba0ac2ca8e248
Adrien Pyke <adrien.pyke@034gc.onmicrosoft.com>
2019-12-10 13:00:15 -0500

    readd function

--- src/util.js
+++ src/util.js
@@ -83,3 +83,20 @@ export const gcd = (x, y) => {
   }
   return x;
 };
+
+export const sortBy = (...cbs) => (a, b) => {
+  for (const cb of cbs) {
+    const aa = cb(a);
+    const bb = cb(b);
+    const diff = cb.desc
+      ? isString(aa)
+        ? bb.localeCompare(aa)
+        : bb - aa
+      : isString(aa)
+      ? aa.localeCompare(bb)
+      : aa - bb;
+    if (diff !== 0) return diff;
+  }
+  return 0;
+};
+export const desc = cb => ((cb.desc = true), cb);
