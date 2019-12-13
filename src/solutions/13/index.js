99ad80e0bc2812cac7f88978ba9c5dc4c47b0ee4
Adrien Pyke <adpyke@gmail.com>
2019-12-13 00:28:29 -0500

    day 13

---
+++ src/solutions/13/index.js
@@ -0,0 +1,51 @@
+import input from './input';
+import intcode from '../intcode';
+import { chunk, InfiniteGrid, output2dArray } from '../../util';
+
+const parseInput = () => input.split(',').map(Number);
+
+const CELLS = {
+  0: ' ',
+  1: '█',
+  2: '#',
+  3: '-',
+  4: '●'
+};
+
+export default {
+  part1() {
+    const grid = new InfiniteGrid(0);
+    chunk([...intcode(parseInput())], 3).map(([x, y, tile]) => grid.set(x, y, tile));
+    return '# of blocks: ' + grid.cells.filter(c => c.value === 2).length;
+  },
+  part2: visualize =>
+    function*() {
+      const grid = new InfiniteGrid(CELLS[0]);
+      const code = parseInput();
+      code[0] = 2;
+
+      let input = 0;
+      let value, done;
+      let paddleX;
+      let ballX;
+      const program = intcode(code);
+      let instruction = [];
+      while (!done) {
+        ({ value, done } = program.next(input));
+        if (value == null) continue;
+        instruction.push(value);
+        if (instruction.length === 3) {
+          const [x, y, tile] = instruction;
+          grid.set(x, y, CELLS[tile] || tile);
+          if (visualize) yield output2dArray(grid.toArray());
+          if (tile === 3) paddleX = x;
+          if (tile === 4) ballX = x;
+          input = paddleX < ballX ? 1 : paddleX > ballX ? -1 : 0;
+          instruction = [];
+        }
+      }
+      yield 'Score: ' + grid.get(-1, 0) + (visualize ? output2dArray(grid.toArray()) : '');
+    },
+  visualize: true,
+  interval: 10
+};
--- src/solutions/index.js
+++ src/solutions/index.js
@@ -10,5 +10,6 @@ import day9 from './09';
 import day10 from './10';
 import day11 from './11';
 import day12 from './12';
+import day13 from './13';
 
-export default [day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12];
+export default [day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13];
